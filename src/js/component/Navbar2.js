9a2d087de1f55800218d560beb08151f163b6d23
cjoy3710 <cathycathyjoyjoy@gmail.com>
2020-11-24 17:41:51 +0000

    navbar update

--- src/js/component/Navbar2.js
+++ src/js/component/Navbar2.js
@@ -26,43 +26,45 @@ export const Navbar2 = () => {
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="mr-auto">
-					<Nav.Link href="#home">Home</Nav.Link>
-					<Nav.Link href="#link">Link</Nav.Link>
+				<Nav className="mr-auto ">
+					<Nav.Link className="nav-link" href="/">
+						Home
+					</Nav.Link>
+					<Nav.Link href="/aboutus">About Us</Nav.Link>
+					<Nav.Link href="/menu">Menu / Place Order</Nav.Link>
 				</Nav>
-				<li className="nav-item">
-					<button type="button" className="btn login-btn" variant="primary" onClick={handleShow}>
-						Login / Sign-Up
-					</button>
-					<Modal show={show} onHide={handleClose}>
-						<Modal.Header closeButton>
-							<Modal.Title>Login Here</Modal.Title>
-						</Modal.Header>
-						<Modal.Body>
-							<Form>
-								<Form.Group controlId="formBasicEmail">
-									<Form.Label>Email address</Form.Label>
-									<Form.Control type="email" placeholder="Enter email" />
-									<Form.Text className="text-muted">
-										Well never share your email with anyone else.
-									</Form.Text>
-								</Form.Group>
 
-								<Form.Group controlId="formBasicPassword">
-									<Form.Label>Password</Form.Label>
-									<Form.Control type="password" placeholder="Password" />
-								</Form.Group>
-								{/* <Form.Group controlId="formBasicCheckbox">
+				<Button type="button" className="btn login-btn" variant="link" onClick={handleShow}>
+					Login / Sign-Up
+				</Button>
+				<Modal show={show} onHide={handleClose}>
+					<Modal.Header closeButton>
+						<Modal.Title>Login Here</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<Form>
+							<Form.Group controlId="formBasicEmail">
+								<Form.Label>Email address</Form.Label>
+								<Form.Control type="email" placeholder="Enter email" />
+								<Form.Text className="text-muted">
+									Well never share your email with anyone else.
+								</Form.Text>
+							</Form.Group>
+
+							<Form.Group controlId="formBasicPassword">
+								<Form.Label>Password</Form.Label>
+								<Form.Control type="password" placeholder="Password" />
+							</Form.Group>
+							{/* <Form.Group controlId="formBasicCheckbox">
 								<Form.Check type="checkbox" label="Check me out" />
 							</Form.Group> */}
-								<Button variant="primary" type="submit">
-									Login
-								</Button>
-							</Form>
-						</Modal.Body>
-						<Modal.Footer />
-					</Modal>
-				</li>
+							<Button variant="primary" type="submit">
+								Login
+							</Button>
+						</Form>
+					</Modal.Body>
+					<Modal.Footer />
+				</Modal>
 			</Navbar.Collapse>
 		</Navbar>
 	);
